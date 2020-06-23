f2fc51c2bf70fe50ed47380cd94d2c135ba5ded9
Laurent Ganne <ganne.laurent@gmail.com>
2020-06-23 12:14:09 +0000

    Getting deployment input property values

---
+++ examples/get-deployment-input-parameters/main.go
@@ -0,0 +1,75 @@
+// Copyright 2020 Bull S.A.S. Atos Technologies - Bull, Rue Jean Jaures, B.P.68, 78340, Les Clayes-sous-Bois, France.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/alien4cloud/alien4cloud-go-client/v2/alien4cloud"
+)
+
+// Command arguments
+var url, user, password, appName string
+
+func init() {
+	// Initialize command arguments
+	flag.StringVar(&url, "url", "http://localhost:8088", "Alien4Cloud URL")
+	flag.StringVar(&user, "user", "admin", "User")
+	flag.StringVar(&password, "password", "changeme", "Password")
+	flag.StringVar(&appName, "app", "", "Name of the application")
+}
+
+func main() {
+
+	// Parsing command arguments
+	flag.Parse()
+
+	// Check required parameter
+	if appName == "" {
+		log.Panic("Mandatory argument 'app' missing (Name of the application)")
+	}
+
+	client, err := alien4cloud.NewClient(url, user, password, "", true)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Timeout after one hour (this is optional you can use a context without timeout or cancelation)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Hour)
+	defer cancel()
+
+	err = client.Login(ctx)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	envID, err := client.ApplicationService().GetEnvironmentIDbyName(ctx, appName, alien4cloud.DefaultEnvironmentName)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	topology, err := client.ApplicationService().GetDeploymentTopology(ctx, appName, envID)
+	if err != nil {
+		log.Panic(err)
+	}
+	deployerInputProperties := topology.Data.Topology.DeployerInputProperties
+	for propName, propVal := range deployerInputProperties {
+		fmt.Printf("Input property %s, value %v\n", propName, propVal.Value)
+	}
+}
